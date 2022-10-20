332f8b450c17d1b56d40954ed8d8aad708578e0f
Saebom Kim <rlatoqha0101@naver.com>
2022-10-20 14:14:38 +0900

    nodejs

--- router/router.js
+++ router/router.js
@@ -1,17 +1,7 @@
-
-const { response } = require("express");
 const express = require("express");
-const mysql = require('mysql');
-
 const router = express.Router();         // express가 가지고있는 기능중에서 라우터 기능 사용을 선언해줌
 
-let conn = mysql.createConnection({     // conn으로 DB에 값을 입력을 할 수 있음
-    host : "127.0.0.1",
-    user : "root",
-    password : "rtq134679@",
-    port : "3306",
-    database : "nodejs_DB"
-});
+const conn = require("../config/DBConfig.js");
 
 router.get("/plus",(request, response) => {    // plus라우터 기능정의 및 등록, get 방식으로 가져옴, plus로 들어올때 함수가 실행됨 -> 미들웨어로 등록해줘야함
     console.log("/plus 라우터 호출");
@@ -121,20 +111,35 @@ router.post('/join', (req, res) => {
 
     res.write("</body>");
     res.write("</html>");
+    res.end();
 });
 
 
 router.post('/Login', (req, res) => {
-    console.log("ID :" + req.body.id);
-    console.log("PW : "+req.body.pw);
-
-    // 사용자가 입력한 id : smatr 이고, pw가 123 일때, 성공 -> Logins.html, 실패 -> Login.html
+/*     console.log("ID :" + req.body.id);
+    console.log("PW : "+req.body.pw); */
+    
     let id = req.body.id;
     let pw = req.body.pw;
-    if (id == 'smart' && pw == '123') {
+
+    let sql = "Select * from member where id = ? and pw = ?";
+
+    conn.query(sql, [id, pw] ,(err, row) => {     // query = 명령을 할 수 있는것 (db에)
+        if(err){
+            console.log("검색이 실패했습니다." + err);
+        } else if (row.length > 0) {
+            // 로그인성공 부분
+            res.redirect ("http://127.0.0.1:5500/nodejs/public/ex05Logins.html");
+        } else if (row.length == 0){
+            // 로그인 실패 부분
+            res.redirect ("http://127.0.0.1:5500/nodejs/public/ex05LoginF.html");
+        }
+    })    
+
+    /* if (id == 'smart' && pw == '123') {
         res.redirect ("http://127.0.0.1:5500/nodejs/public/ex05Logins.html");       // 경로 재설정해줌
     } else 
-        res.redirect ("http://127.0.0.1:5500/nodejs/public/ex05LoginF.html");
+        res.redirect ("http://127.0.0.1:5500/nodejs/public/ex05LoginF.html"); */
 
     
 });
@@ -152,11 +157,19 @@ router.post('/JoinDB', (req, res) => {
     let sql = "insert into member values(?, ?, ?)"; // ?의 순서대로 아래 [] 안에 적어주기
 
     conn.query(sql, [id, pw, nick] ,(err, row) => {     // query = 명령을 할 수 있는것 (db에)
-        if(!err){
-            console.log("입력성공 : "+row);
+        if(err){
+            console.log("입력실패 : "+ err)
+
+            console.log("입력성공 : "+row.affectedRows);
+            
+        } else if (row.affectedRows > 0) {
+
+            console.log("명령에 성공한 수 : "+ row.affectedRows);
             res.redirect("http://127.0.0.1:5500/nodejs/public/ex06Main.html");
-        } else{
-            console.log("입력실패 : "+ err);
+
+        } else if(row.affectedRows == 0){
+
+            console.log("삭제된 값이 없습니다.")
         }
     })
     
@@ -178,11 +191,124 @@ router.get('/Delete', (req, res) => {
             res.redirect("http://127.0.0.1:5500/nodejs/public/ex06Main.html");
         } else if (row.affectedRows == 0) {
             console.log("삭제된 값이 없습니다.")
-        } else{
+        } else {
             console.log("삭제실패 : "+ err);
         }
     })
 
+});
+
+
+router.post('/Update', (req, res) => {
+
+    // 사용자가 입력한 id 의 pw를 변경하고
+    // 성공 후 Main.html페이지로 이동하시오.
+    let id = req.body.id;
+    let select = req.body.select;       // pw 아니면 nick이 넘어옴
+    let data = req.body.data;           // 변경될 데이터가 넘어옴
+
+    let sql;                            // if문이 실행이되면 빠져나올때 선언했던 지역변수가 사라지므로
+    if (select == 'pw') {               // 'pw'로 하는 이유는 변수명으로 오는게아니라 text로 넘어오기 때문
+        sql = "update member set pw = ? where id = ?";
+       
+    } else if (select == 'nick') {
+        sql = "update member set nick = ? where id = ?"      // set 어떤데이터를 where 어떻게 
+    } 
+
+    conn.query(sql, [data, id] , (err, row) => {
+        if (err) {
+            console.log("수정이 실패했습니다.");
+        } else if (row.affectedRows > 0){
+            console.log("수정이 성공했습니다." + row.affectedRows);
+            res.redirect("http://127.0.0.1:5500/nodejs/public/ex06Main.html");
+        } else if (row.affectedRows == 0) {
+            console.log(" 수정된 값이 없습니다.")
+        }
+    })
+
+
+});
+
+router.get('/selectAll', (req, res) => {
+
+
+    let sql = "select * from member"; // 전체 데이터 불러오기
+
+    conn.query(sql, (err, row) => {     // query = db에 명령을 할 수 있는것
+
+
+        if(err){
+            console.log("검색실패 : "+ err);
+        } else if(row.length > 0) {
+            console.log("검색된 데이터의 수 : "+ row.length)
+
+            res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
+            res.write("<html>");
+            res.write("<bodyl>");
+            
+            res.write("<table border='1'>")     /* berder : 테이블 선을 만들어줌 (표처럼) */
+            res.write("<tr>")
+            res.write("<th>ID</th>")
+            res.write("<th>PW</th>")
+            res.write("<th>NICK</th>")
+            res.write("</tr>")
+
+            for(let i=0; i<row.length; i++){
+                res.write("<tr>")
+                res.write("<td>"+row[i].id+"</td>");
+                res.write("<td>"+row[i].pw+"</td>");
+                res.write("<td>"+row[i].nick+"</td>");
+                res.write("</tr>")
+            }
+            res.write("</table>")
+            res.write("</body>");
+            res.write("</html>");
+            res.end();
+
+        } else if (row.length == 0){
+            console.log("검색된 데이터가 없습니다.")
+        }
+})
+
+});
+
+// 회원 검색 라우터 만들기
+// 1. get방식의 /SlectOne라우터 생성
+// 2. 사용자가 입력한 id의 정보만 검색해 브라우저 출력
+router.get('/SelectOne', (req, res) => {
+    
+    let id = req.query.id;
+    let sql = "Select * from member where id = ?";
+
+    conn.query(sql, [id] ,(err, row) => {     // query = 명령을 할 수 있는것 (db에)
+        if(err){
+            console.log("검색이 실패했습니다.")
+        } else if (row.length > 0) {
+            console.log("검색된 데이터의 수 : "+row.length)
+
+            res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
+            res.write("<html>");
+            res.write("<body>");
+            
+            res.write("<table border='1'>")     /* berder : 테이블 선을 만들어줌 (표처럼) */
+            res.write("<tr>")
+            res.write("<th>ID</th>")
+            res.write("</tr>")
+
+            for(let i=0; i<row.length; i++){
+                res.write("<tr>")
+                res.write("<td>"+row[i].id+"</td>");
+                res.write("</tr>")
+            }
+            res.write("</table>")
+            res.write("</body>");
+            res.write("</html>");
+            res.end();
+
+        } else if (row.length == 0){
+            console.log("검색된 값이 없습니다.");
+        }
+    })
 });
 
 // 외부에서 사용할 수 있게 만들어주기
